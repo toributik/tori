8a2c7fc9f3465efbf6a74bf8eeee6b93eb7f9271
toributik <toributik33@gmail.com>
2025-10-15 20:28:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+//     const scriptURL = "https://script.google.com/macros/s/AKfycby_8YbgujWGwNA0WM2ZsRgprs8GRDCcPzpaTtJ6p7MTQdvI7KamPkktY-NGmOH5-6gb/exec";
+
+
+//    // Configuration for order.html
+//     const CONFIG = {
+//       scriptURL: "https://script.google.com/macros/s/AKfycby_8YbgujWGwNA0WM2ZsRgprs8GRDCcPzpaTtJ6p7MTQdvI7KamPkktY-NGmOH5-6gb/exec",
+//       timeout: 10000, // 10 seconds
+//       maxRetries: 2
+//     };
+
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec";
+
+fetch(`${scriptURL}?action=addProduct&name=Test&key=MY_SECRET_KEY`)
+  .then(res => res.text())
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
+
+
+
+   // Configuration for order.html
+    const CONFIG = {
+      scriptURL: "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec",
+      timeout: 10000, // 10 seconds
+      maxRetries: 2
+    };
