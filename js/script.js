1e0b472c01ee641b5d7ff64261b0a444a6d8de1b
toributik <toributik33@gmail.com>
2025-10-15 21:22:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,18 +11,19 @@
 //     };
 
 
-const scriptURL = "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec";
+// const scriptURL = "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec";
 
-fetch(`${scriptURL}?action=addProduct&name=Test&key=MY_SECRET_KEY`)
-  .then(res => res.text())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+// fetch(`${scriptURL}?action=addProduct&name=Test&key=MY_SECRET_KEY`)
+//   .then(res => res.text())
+//   .then(data => console.log(data))
+//   .catch(err => console.error(err));
 
 
 
-   // Configuration for order.html
-    const CONFIG = {
-      scriptURL: "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec",
-      timeout: 10000, // 10 seconds
-      maxRetries: 2
-    };
+//    // Configuration for order.html
+//     const CONFIG = {
+//       scriptURL: "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec",
+//       timeout: 10000, // 10 seconds
+//       maxRetries: 2
+
+//     };
