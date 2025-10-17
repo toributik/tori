bd77bbb642eca94134883dff0fb7c86b680a5fb7
toributik <toributik33@gmail.com>
2025-10-17 15:45:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,6 @@
      const scriptURL = "https://script.google.com/macros/s/AKfycby_8YbgujWGwNA0WM2ZsRgprs8GRDCcPzpaTtJ6p7MTQdvI7KamPkktY-NGmOH5-6gb/exec";
 
 
-//    // Configuration for order.html
-//     const CONFIG = {
-//       scriptURL: "https://script.google.com/macros/s/AKfycby_8YbgujWGwNA0WM2ZsRgprs8GRDCcPzpaTtJ6p7MTQdvI7KamPkktY-NGmOH5-6gb/exec",
-//       timeout: 10000, // 10 seconds
-//       maxRetries: 2
-//     };
-
-
-
- //   const scriptURL = "https://script.google.com/macros/s/AKfycbxdjJhsHzqgWhuXWIDDJGGUT-sYz7iiJ4BzDUU3RaZvmFtDuHeU0i5AFD_7yKfZsX9LCQ/exec";
 
 
 
